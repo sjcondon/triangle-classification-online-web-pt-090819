9b7e739e555038a761c60349dbeb9524271bba05
sarah <s.j.condon01@gmail.com>
2019-10-16 00:26:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   end
 
   def kind
-    if((@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)) || ((@side_1 + @side_2 <= @side_3) || (@side_2 + @side_3 <= @side_1) || (@side_1 + @side_2 <= @side_3) || (@side_1 + @side_2 <= @side_3) || )
+    if((@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)) || ((@side_1 + @side_2 <= @side_3) || (@side_2 + @side_3 <= @side_1) || (@side_1 + @side_2 <= @side_3) || (@side_3 + @side_1 <= @side_2))
       raise TriangleError
     elsif @side_1 == @side_2 && @side_2 == @side_3
       :equilateral
