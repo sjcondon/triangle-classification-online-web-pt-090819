ab55f36021c74a92602838baca8a112455b9ae7f
sarah <s.j.condon01@gmail.com>
2019-10-16 00:31:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
     @side_3 = side_3
   end
 
-  def kindlear
+  def kind
     if((@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)) || ((@side_1 + @side_2 <= @side_3) || (@side_2 + @side_3 <= @side_1) || (@side_1 + @side_2 <= @side_3) || (@side_3 + @side_1 <= @side_2))
       raise TriangleError
     elsif @side_1 == @side_2 && @side_2 == @side_3
